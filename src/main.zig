e70d56f5bf4f4c592e08bf1f678c1ae4b68d4509
Riccardo Binetti <rbino@gmx.com>
2021-04-03 19:03:23 +0200

    Add systemInit function
    Perform the same initialization that is done in libopencm3

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,79 @@
+const regs = @import("registers.zig");
+
 export fn main() void {
+    systemInit();
+
     var i: u32 = 0;
     while (true) {
         i += 1;
     }
+}
+
+fn systemInit() void {
+    // Enable FPU coprocessor
+    // WARN: currently not supported in qemu, comment if testing it there
+    regs.FPU_CPACR.CPACR.modify(.{ .CP = 0b11 });
+
+    // Enable HSI
+    regs.RCC.CR.modify(.{ .HSION = 1 });
+
+    // Wait for HSI ready
+    while (regs.RCC.CR.read().HSIRDY != 1) {}
+
+    // Select HSI as clock source
+    regs.RCC.CFGR.modify(.{ .SW0 = 0, .SW1 = 0 });
+
+    // Enable external high-speed oscillator (HSE)
+    regs.RCC.CR.modify(.{ .HSEON = 1 });
+
+    // Wait for HSE ready
+    while (regs.RCC.CR.read().HSERDY != 1) {}
+
+    // Set prescalers for 168 MHz: HPRE = 0, PPRE1 = DIV_2, PPRE2 = DIV_4
+    regs.RCC.CFGR.modify(.{ .HPRE = 0, .PPRE1 = 0b101, .PPRE2 = 0b100 });
+
+    // Disable PLL before changing its configuration
+    regs.RCC.CR.modify(.{ .PLLON = 0 });
+
+    // Set PLL prescalers and HSE clock source
+    // TODO: change the svd to expose prescalers as packed numbers instead of single bits
+    regs.RCC.PLLCFGR.modify(.{
+        .PLLSRC = 1,
+        // PLLM = 8 = 0b001000
+        .PLLM0 = 0,
+        .PLLM1 = 0,
+        .PLLM2 = 0,
+        .PLLM3 = 1,
+        .PLLM4 = 0,
+        .PLLM5 = 0,
+        // PLLN = 336 = 0b101010000
+        .PLLN0 = 0,
+        .PLLN1 = 0,
+        .PLLN2 = 0,
+        .PLLN3 = 0,
+        .PLLN4 = 1,
+        .PLLN5 = 0,
+        .PLLN6 = 1,
+        .PLLN7 = 0,
+        .PLLN8 = 1,
+        // PLLP = 2 = 0b10
+        .PLLP0 = 0,
+        .PLLP1 = 1,
+        // PLLQ = 7 = 0b111
+        .PLLQ0 = 1,
+        .PLLQ1 = 1,
+        .PLLQ2 = 1,
+    });
+
+    // Enable PLL
+    regs.RCC.CR.modify(.{ .PLLON = 1 });
+
+    // Wait for PLL ready
+    while (regs.RCC.CR.read().PLLRDY != 1) {}
+
+    // Enable flash data and instruction cache and set flash latency to 5 wait states
+    regs.FLASH.ACR.modify(.{ .DCEN = 1, .ICEN = 1, .LATENCY = 5 });
+
+    // Disable HSI
+    regs.RCC.CR.modify(.{ .HSION = 0 });
 }
