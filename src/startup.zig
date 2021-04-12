c9127952c3c465c087cbd734f9daccd8d1c3c0c3
Riccardo Binetti <rbino@gmx.com>
2021-04-12 22:20:57 +0200

    Cleanup startup code and linker
    Calculate data and bss size from end and start symbols in Zig, without adding
    the size symbol explicitly in the linker script. Also try to use const where
    possible in the startup code.

--- src/startup.zig
+++ src/startup.zig
@@ -1,22 +1,22 @@
 usingnamespace @import("main.zig");
 
 // These symbols come from the linker script
-extern var _data_loadaddr: u32;
+extern const _data_loadaddr: u32;
 extern var _data: u32;
-extern var _data_size: u32;
+extern const _edata: u32;
 extern var _bss: u32;
-extern var _bss_size: u32;
+extern const _ebss: u32;
 
 export fn resetHandler() void {
     // Copy data from flash to RAM
-    var data_loadaddr = @ptrCast([*]u8, &_data_loadaddr);
-    var data = @ptrCast([*]u8, &_data);
-    var data_size = @ptrToInt(&_data_size);
+    const data_loadaddr = @ptrCast([*]const u8, &_data_loadaddr);
+    const data = @ptrCast([*]u8, &_data);
+    const data_size = @ptrToInt(&_edata) - @ptrToInt(&_data);
     for (data_loadaddr[0..data_size]) |d, i| data[i] = d;
 
     // Clear the bss
     const bss = @ptrCast([*]u8, &_bss);
-    const bss_size = @ptrToInt(&_bss_size);
+    const bss_size = @ptrToInt(&_ebss) - @ptrToInt(&_bss);
     for (bss[0..bss_size]) |*b| b.* = 0;
 
     // Call main imported from main.zig
